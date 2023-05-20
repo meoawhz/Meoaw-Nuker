e1b21968e6a02053656c7cba7751e741024fd558
Meoaw <129369441+exaDev99@users.noreply.github.com>
2023-05-20 22:07:07 +0700

    Add new config

--- gotji.py
+++ gotji.py
@@ -1,5 +1,6 @@
 main = {
-    "token": "",
-    "chid":"",
-    "msg":"@everyone @here\n \nEasy by pass | **Meoaw Nuke**",
+    "token": "", # โทเคน บช ดิส ของเรา
+    "chid":"", # ช่องที่จะให้สแปม
+    "sv_id":"", # ไอดีเซิฟเวอร์ดิส
+    "msg":"@everyone @here\n \nEasy by pass | **Meoaw Nuke**", # ข้อความที่ต้องการให้สแปม
 }
